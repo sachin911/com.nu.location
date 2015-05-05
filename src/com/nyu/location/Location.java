5baf25c5346bf1ff10f1f197b77108f775359d85
sachin <sn1628@nyu.edu>
2015-05-04 21:31:58 -0400

    NYU_LocationApp Web Service
    This is the web service for the address verification part of the app

---
+++ src/com/nyu/location/Location.java
@@ -0,0 +1,42 @@
+package com.nyu.location;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("/address")
+public class Location {
+
+	 @GET
+	  @Produces(MediaType.APPLICATION_JSON)
+	  public String checkLocationValidity(@QueryParam("address") String addr) {
+		
+		// JSONObject obj = new JSONObject();
+		 String response;
+		 if(checkaddress(addr)) {
+			 //obj.put("Location", "Office");
+			 response = ConstructJSON.constructJSON("Yes Office", true);
+		 } else {
+			 //obj.put("location", "not office");
+			 response = ConstructJSON.constructJSON("NOT Office", false);
+		 }
+		 System.out.println("----*****-----"+response);
+	    return response;
+	  }
+	 
+	 public boolean checkaddress(String address) {
+		 boolean flag;
+		 if(address.contains("2685")) {
+			 flag = true;
+		 } else {
+			 flag = false;
+		 }
+		 return flag;
+		 
+	 }
+	 
+	
+}
